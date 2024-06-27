34fe009af5ee758f6081b9ef6cfb73483f58b1d5
Ana Godoy Papst <anapapst@Anas-Laptop.local>
2024-06-27 20:04:46 -0300

    Project Init

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>Your Daily Dish A Food Journey</p>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam,
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
